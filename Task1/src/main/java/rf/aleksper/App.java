4526d6102bb06428a4d6fd60d0508f72212ea169
alexper2022 <109676776+alexper2022@users.noreply.github.com>
2024-01-28 16:24:16 +0300

    create Task1

---
+++ Task1/src/main/java/rf/aleksper/App.java
@@ -0,0 +1,13 @@
+package rf.aleksper;
+
+import java.util.stream.IntStream;
+
+/*
+  1. Напишите программу, которая использует Stream API для обработки списка чисел.
+     Программа должна вывести на экран среднее значение всех четных чисел в списке.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
