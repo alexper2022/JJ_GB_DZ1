7fee1c55aa548e17d9dd19e30cb9d56c9c7b2eb9
alexper2022 <109676776+alexper2022@users.noreply.github.com>
2024-01-28 16:24:37 +0300

    Task1 ended

--- Task1/src/main/java/rf/aleksper/App.java
+++ Task1/src/main/java/rf/aleksper/App.java
@@ -8,6 +8,22 @@ import java.util.stream.IntStream;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("");
+
+        int[] array = new int[]{5, 3, 7, 5, 3, 1, 55, 323, 23, 4, 9, 67, 8, 98, 54, 90};
+        
+        // Без Stream API
+        double summ = 0;
+        int count = 0;
+        for (int n : array) {
+            if (n % 2 == 0) {
+                summ += n;
+                count++;
+            }
+        }
+        System.out.println(summ / count);
+
+        // Со Stream API
+        double avrg = IntStream.of(array).filter(n -> n % 2 == 0).average().getAsDouble();
+        System.out.println(avrg);
     }
 }
